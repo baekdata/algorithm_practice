6eb69831a27e7e1dfaf3e52d1bc5bf9732e987ee
jongsubaek <wowterry9044@gmail.com>
2018-09-20 00:08:32 +0900

    solve the mergeSum

--- Algorithm_Practice/src/Study180913/BJ_2231_mergeSum.java
+++ Algorithm_Practice/src/Study180913/BJ_2231_mergeSum.java
@@ -1,9 +1,6 @@
 package Study180913;
-
 import java.util.Scanner;
-
 public class BJ_2231_mergeSum {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
@@ -12,18 +9,16 @@ public class BJ_2231_mergeSum {
 		//		int size = numString.length();
 
 		// ex 19
-
 		calc(num);
 	}
 
 	public static void calc(int num) {
 		int sum = 0;	
 		int n = 0;
-
+		boolean isOk = false;
 		for(int i=1; i<=1000000; i++) {
 			sum = i;
 			n = i;
-			
 			while(n!=0) {
 				sum += n%10;
 				n = n/10;
@@ -31,9 +26,13 @@ public class BJ_2231_mergeSum {
 			
 			if(sum == num) {
 				System.out.println(i);
-			}else {
-				System.out.println(0);
+				n=0;
+				isOk = true;
+				break;
 			}
+		}
+		if(!isOk) {
+			System.out.println(0);
 		}
 	}
 }
