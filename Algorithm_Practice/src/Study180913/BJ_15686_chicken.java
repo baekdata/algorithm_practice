14bdbcb3d98d45a20758afc53f4bdd7c2dcb1e07
jongsubaek <wowterry9044@gmail.com>
2018-09-19 23:59:17 +0900

    solve problem - brute force

---
+++ Algorithm_Practice/src/Study180913/BJ_15686_chicken.java
@@ -0,0 +1,17 @@
+package Study180913;
+
+import java.util.Scanner;
+
+public class BJ_15686_chicken { // 백준, 치킨배달 - 브루트포스 
+	static int[][] map;
+	static int n, m, minimumResult;
+	
+	public static void main() {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+		
+		System.out.println(minimumResult);
+	}
+}
---
+++ Algorithm_Practice/src/Study180913/BJ_2231_mergeSum.java
@@ -0,0 +1,39 @@
+package Study180913;
+
+import java.util.Scanner;
+
+public class BJ_2231_mergeSum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		//		String numString = num+"";
+		//		int size = numString.length();
+
+		// ex 19
+
+		calc(num);
+	}
+
+	public static void calc(int num) {
+		int sum = 0;	
+		int n = 0;
+
+		for(int i=1; i<=1000000; i++) {
+			sum = i;
+			n = i;
+			
+			while(n!=0) {
+				sum += n%10;
+				n = n/10;
+			}
+			
+			if(sum == num) {
+				System.out.println(i);
+			}else {
+				System.out.println(0);
+			}
+		}
+	}
+}
